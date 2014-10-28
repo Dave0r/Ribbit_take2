4d5cdf164042a4cc899aade6cf83f03508acd4c3
Dave0r <david.barnes@aria-networks.com>
2014-10-28 13:47:30 +0000

    fucking finally - I hope

---
+++ app/src/main/java/com/wonderdave/david/ribbit/ParseConstants.java
@@ -0,0 +1,14 @@
+package com.wonderdave.david.ribbit;
+
+/**
+ * Created by david on 28/10/14.
+ */
+public final class ParseConstants {
+
+    //class names
+
+
+    //Field Names
+    public static final String KEY_USERNAME = "username";
+
+}
